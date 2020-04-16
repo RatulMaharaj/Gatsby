ff9b207a9e09a75e4a0c51da858ff53d9aa8ea8e
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-04-16 12:04:58 +0200

    Moved Covid Stats into component

--- src/components/covid.js
+++ src/components/covid.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
 
-export default () => (
+export default () => {
+  // Client-side Runtime Data Fetching
+  const url = `https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=South%20Africa`
+
+  const [Deaths, setDeaths] = useState(0)
+  const [Confirmed, setConfirmed] = useState(0)
+  const [Recoveries, setRecoveries] = useState(0)
+  const [LastUpdate, setLastUpdate] = useState(0)
+
+  useEffect(() => {
+    // get data using fetch
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+        "x-rapidapi-key": "d44a34af09msh87c6734aee1c3f7p14e9e3jsn9dcb321a4dc7",
+      },
+    }).then(response => response.json()) // parse JSON from request
+    .then(resultData =>{
+        setDeaths(resultData["data"]["covid19Stats"][0]["deaths"])
+        setConfirmed(resultData["data"]["covid19Stats"][0]["confirmed"])
+        setRecoveries(resultData["data"]["covid19Stats"][0]["recovered"])
+        setLastUpdate(resultData["data"]["covid19Stats"][0]["lastUpdate"])
+    })
+  })
+  
+  return (
     <div>
-        <h3>Covid-19 Stats (ZA)</h3>
-        <br/>
-        <ul className="alt">
-                <li>
-                    <h4>Confirmed cases: <q id="confirmed"></q></h4>
-                </li>
-                <li>
-                    <h4>Deaths: <q id="deaths"></q></h4>
-                </li>
-                <li>
-                    <h4>Recoveries: <q id="recovered"></q></h4>
-                </li>
-        </ul>
-        <h4>Last updated on <q id="lastupdate"></q></h4>	
+      <h3>Covid-19 Stats (ZA)</h3>
+      <br />
+      <ul className="alt">
+        <li>
+            <h4>Confirmed cases: {Confirmed}</h4>
+        </li>
+        <li>
+          <h4>Deaths: {Deaths}</h4>
+        </li>
+        <li>
+          <h4>
+            Recoveries: {Recoveries}
+          </h4>
+        </li>
+      </ul>
+      <h4>
+        Last updated on {LastUpdate}
+      </h4>
     </div>
-)
+  )
+}
