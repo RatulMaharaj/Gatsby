79a2797ae2490cb9a9ffdf5d360fb6fcb4285964
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-04-18 00:16:06 +0200

    updated rss footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ export default () => (
             <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/RatulMaharaj/" className="icon brands fa-github"><span className="label">Github</span></a></li>
             <li><a target="_blank" rel="noopener noreferrer" href="mailto:blog@ratulmaharaj.com" className="icon solid fa-envelope"><span
                     className="label">Email</span></a></li>
-            <li><Link target="_blank" rel="noopener noreferrer" to="/rss.xml" className="icon solid fa-rss"><span
+            <li><Link target="_blank" rel="noopener noreferrer" to="/rss-feed.xml" className="icon solid fa-rss"><span
                     className="label">RSS</span></Link></li>
         </ul>
         <p className="copyright">Ratul Maharaj</p>
