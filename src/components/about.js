53a6393bcd9d8352193be2ffc43dd37520e17208
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-04-17 12:57:20 +0200

    Fixed sp in about

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ export default () => (
         <p>
             Hi there! My name is Ratul. I'm an actuarial student with passion for creativity and problem solving.
             I also have a keen interest in lots of random things such as coding, photography and learning to
-            play guitar. I'm always excited to learn and share, so i hope that you find something useful here!
+            play guitar. I'm always excited to learn and share, so I hope that you find something useful here!
         </p>
     </section>
 )
