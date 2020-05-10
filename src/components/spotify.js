714d81192f4ca549021eb717b3e2669b35700aef
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-05-10 23:32:11 +0200

    Tweaked spotify description

--- src/components/spotify.js
+++ src/components/spotify.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
     <section className="blurb">
         <h2>Listen to my Spotify</h2>
-        <p>Here's a playlist of some of my favourite songs!</p>
+        <p>Here's a playlist of some of my favourite songs that I've discovered on Spotify.</p>
         <iframe title="ListenWithMe" src="https://open.spotify.com/embed/playlist/0UKekm6bKMyFm4WfeNtofM" width="100%" height="480" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </section>
 )
